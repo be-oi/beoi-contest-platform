764e3b872939134b2765ca56ba4e8e5ad10bf46d
Damien Leroy <damien.leroy@be-oi.be>
2016-11-05 16:30:41 +0100

    add blocly to copied assets

--- bin/upload_static_to_s3.sh
+++ bin/upload_static_to_s3.sh
@@ -20,12 +20,9 @@ i18n/en/translation.json
 "
 for file in $FILES
 do
-	aws s3 cp contestInterface/$file s3://contest-platform-contests-eu-central-1-997893130250/contestAssets/$file --acl public-read $@ --cache-control 'max-age=60, must-revalidate'
+	aws s3 cp contestInterface/$file s3://contest-platform-contests-eu-central-1-997893130250/contestAssets/$file --acl public-read --cache-control 'max-age=60, must-revalidate' $@ 
 done
+aws s3 cp --recursive ext/blockly/ s3://contest-platform-contests-eu-central-1-997893130250/contestAssets/blockly --acl public-read --cache-control 'max-age=60, must-revalidate' $@
 
 # gzip -c9 translations/castor-contest.json | aws s3 cp - s3://static3.castor-informatique.fr/contestAssets/i18n/fr/castor.json --acl public-read --content-encoding 'gzip' --content-type 'application/json' --region eu-west-1 --cache-control 'max-age=43200, must-revalidate'
 # i18n/fr/translation.json
-# 
-# gzip -c9 beaver_tasks/modules/ext/blockly/handclosed.cur | aws s3 cp - s3://static3.castor-informatique.fr/contestAssets/blockly/handclosed.cur --acl public-read --content-encoding 'gzip' --content-type 'image/x-win-bitmap' --region eu-west-1 --cache-control 'max-age=43200, must-revalidate'
-# gzip -c9 beaver_tasks/modules/ext/blockly/handopen.cur | aws s3 cp - s3://static3.castor-informatique.fr/contestAssets/blockly/handopen.cur --acl public-read --content-encoding 'gzip' --content-type 'image/x-win-bitmap' --region eu-west-1 --cache-control 'max-age=43200, must-revalidate'
-# gzip -c9 beaver_tasks/modules/ext/blockly/sprites.png | aws s3 cp - s3://static3.castor-informatique.fr/contestAssets/blockly/sprites.png --acl public-read --content-encoding 'gzip' --content-type 'image/png' --region eu-west-1 --cache-control 'max-age=43200, must-revalidate'
