2254293b370c66f8f7beb28bc201eece3a3bc66f
Damien Leroy <damien.leroy@be-oi.be>
2016-11-19 19:46:28 +0100

    add new json files to static files

--- bin/upload_static_to_s3.sh
+++ bin/upload_static_to_s3.sh
@@ -16,12 +16,17 @@ bower_components/pem-platform/task-pr.js
 style.css
 raphael-min.js
 i18n/fr/translation.json
-i18n/en/translation.json
+i18n/fr/be.json
+i18n/nl/translation.json
+i18n/nl/be.json
+i18n/de/translation.json
+i18n/de/be.json
 "
 for file in $FILES
 do
 	aws s3 cp contestInterface/$file s3://contest-platform-contests-eu-central-1-997893130250/contestAssets/$file --acl public-read --cache-control 'max-age=60, must-revalidate' $@ 
 done
+
 aws s3 cp --recursive ext/blockly/ s3://contest-platform-contests-eu-central-1-997893130250/contestAssets/blockly --acl public-read --cache-control 'max-age=60, must-revalidate' $@
 
 # gzip -c9 translations/castor-contest.json | aws s3 cp - s3://static3.castor-informatique.fr/contestAssets/i18n/fr/castor.json --acl public-read --content-encoding 'gzip' --content-type 'application/json' --region eu-west-1 --cache-control 'max-age=43200, must-revalidate'
