77a911043d9c2fb2aed421c127979c0cbfaa65d7
Damien Leroy <damien.leroy@be-oi.be>
2017-01-23 20:35:52 +0100

    add phone numbers to notice

--- config/config_fr.php
+++ config/config_fr.php
@@ -14,5 +14,7 @@ $config->teacherInterface->baseUrl = 'https://gestion.be-oi.be';
 $config->teacherInterface->sCoordinatorFolder = 'https://gestion.be-oi.be';
 $config->teacherInterface->sAbsoluteStaticPathOldIE = 'https://gestion.be-oi.be';
 
+$config->teacherInterface->sHotlineNumber = "0474/353999 (Damien Leroy)";
+
 $config->validationMailBody = "Bonjour,\r\n\r\nPour valider votre inscription en tant que coordinateur pour le concours beOI, ouvrez le lien suivant dans votre navigateur  : \r\n\r\n%s\r\n\r\nN'hésitez pas à nous contacter si vous rencontrez des difficultés.\r\n\r\nCordialement,\r\n-- \r\nL'équipe beOI";
 $config->validationMailTitle = "Éliminatoires beOI : Confirmation d'inscription";
